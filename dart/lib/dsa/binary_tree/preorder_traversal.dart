0a19f329c636b33d5b406345912bbd96da54f2b7
Daisuke Takahashi <daisuke332003@gmail.com>
2025-09-25 16:09:11 -0700

    Add Preorder Traversal to Dart.

--- dart/lib/dsa/binary_tree/preorder_traversal.dart
+++ dart/lib/dsa/binary_tree/preorder_traversal.dart
@@ -1,9 +1,9 @@
-// lib/dsa/binary_tree/inorder_traversal.dart
+// lib/dsa/binary_tree/preorder_traversal.dart
 
 import 'package:coding_challenges/dsa/binary_tree/binary_tree.dart';
 
 class Solution {
-  List<int> inorderTraversal(TreeNode? root) {
+  List<int> preorderTraversal(TreeNode? root) {
     if (root == null) {
       return [];
     }
@@ -14,13 +14,13 @@ class Solution {
 
     while (current != null || stack.isNotEmpty) {
       while (current != null) {
+        result.add(current.val);
         stack.add(current);
         current = current.left;
       }
 
       // current is null
       current = stack.removeLast();
-      result.add(current.val);
 
       current = current.right;
    }
---
+++ dart/test/dsa/binary_tree/preorder_traversal_test.dart
@@ -0,0 +1,50 @@
+// test/binary_tree/inorder_traversal_test.dart
+
+import 'package:test/test.dart';
+
+import 'package:coding_challenges/dsa/binary_tree/binary_tree.dart';
+import 'package:coding_challenges/dsa/binary_tree/preorder_traversal.dart';
+
+void main() {
+  group('Preorder Traversal', () {
+    test('[1,null,2,3]', () {
+      var list = <int?>[1, null, 2, 3];
+      var tree = buildBinaryTree(list);
+
+      var s = Solution();
+      var result = s.preorderTraversal(tree);
+
+      expect(result, equals([1, 2, 3]));
+    });
+
+    test('[1,2,3,4,5,null,8,null,null,6,7,9]', () {
+      var list = <int?>[1, 2, 3, 4, 5, null, 8, null, null, 6, 7, 9];
+      var tree = buildBinaryTree(list);
+
+      var s = Solution();
+      var result = s.preorderTraversal(tree);
+
+      expect(result, equals([1, 2, 4, 5, 6, 7, 3, 8, 9]));
+    });
+
+    test('[]', () {
+      var list = <int?>[];
+      var tree = buildBinaryTree(list);
+
+      var s = Solution();
+      var result = s.preorderTraversal(tree);
+
+      expect(result, equals([]));
+    });
+
+    test('[1]', () {
+      var list = <int?>[1];
+      var tree = buildBinaryTree(list);
+
+      var s = Solution();
+      var result = s.preorderTraversal(tree);
+
+      expect(result, equals([1]));
+    });
+  });
+}
